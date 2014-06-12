b8cbb8cd07dd610997f19233d21acb73440c94b6
Andreas Weller <andreas.m.weller@gmail.com>
2014-06-12 10:53:19 +0100

    Removed legacy files.

--- test_annotation.py
+++
@@ -1,8 +0,0 @@
-import AnnotationRowParsers as AP
-import SampleAnnotation as SA
-import sys
-
-infile = sys.argv[1]
-
-sample = SA.SampleAnnotation(infile, target_folder= "", run_tools=True)
-sample.print_rows()
